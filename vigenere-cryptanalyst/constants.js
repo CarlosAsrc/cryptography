68fe6796aa22441fbcf7247b16a82cb0b05e9172
CarlosAsrc <carlos.asrc@gmail.com>
2021-04-24 20:52:06 -0300

    Adds support to multiple languages. Create README.md. Create terminal interaction.

--- vigenere-cryptanalyst/constants.js
+++ vigenere-cryptanalyst/constants.js
@@ -1,9 +1,13 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 const englishCoincidenceIndex = 0.0667
+const portugueseCoincidenceIndex = 0.072723
 const englishMostFrequentLetter = 'e'
+const portugueseMostFrequentLetter = 'a'
 
 module.exports = {
     alphabet,
     englishCoincidenceIndex,
-    englishMostFrequentLetter
+    englishMostFrequentLetter,
+    portugueseCoincidenceIndex,
+    portugueseMostFrequentLetter
 }
--- vigenere-cryptanalyst/vigenere-decryptor.js
+++ vigenere-cryptanalyst/vigenere-decryptor.js
@@ -1,12 +1,16 @@
+#!/usr/bin/env node
 const yargs = require('yargs');
+const readline = require('readline-sync')
+const chalk = require('chalk')
+
 const FileUtils = require('./file-utils');
 const CoincidenceIndexService = require('./coincidence-index-service');
-const {alphabet, englishCoincidenceIndex, englishMostFrequentLetter} = require('./constants');
+const {alphabet, englishCoincidenceIndex, englishMostFrequentLetter, portugueseCoincidenceIndex, portugueseMostFrequentLetter} = require('./constants');
 
 yargs.command(
     {
         command: 'decrypt',
-        describe: 'Add new note',
+        describe: 'Decifrar uma Cifra de Vigenère',
         builder: {
             cipher: {
                 describe: 'Texto cifrado',
@@ -17,9 +21,21 @@ yargs.command(
                 describe: 'Arquivo com texto cifrado',
                 demandOption: false,
                 type: 'string'
-            }
+            },
+            language: {
+                describe: 'Língua do texto claro (suportados: en, pt)',
+                demandOption: true,
+                type: 'string'
+            },
         },
         handler(argv) {
+            if(argv.language == 'pt') {
+                languageCoincidenceIndex = portugueseCoincidenceIndex
+                languageMostFrequentLetter = portugueseMostFrequentLetter
+            } else if (argv.language == 'en') {
+                languageCoincidenceIndex = englishCoincidenceIndex
+                languageMostFrequentLetter = englishMostFrequentLetter
+            }
             if(argv.file) {
                 decrypt(FileUtils.load(argv.file).toLowerCase().split(/\s/).join(''), argv.file)    
             } else if(argv.cipher){
@@ -29,24 +45,31 @@ yargs.command(
     }
 )
 
-
+let languageCoincidenceIndex
+let languageMostFrequentLetter
 
 const decrypt = (cipherText, file) => {
-    let output = ''
-    const keySize = calculatekeySize(cipherText);
-    const key = discoverKey(cipherText, keySize);
-    const clearText = getClearText(cipherText, key);
-    
+    let output = '', key ='', clearText=''
+    const possibleKeys = discoverPossibleKeys(cipherText, calculatekeySize(cipherText));
 
-    console.log('Key Size: ' + keySize)
-    console.log('Key: ' + key)
-    console.log('Clear text: \n' + clearText)
-
-    output += 'Key Size: ' + keySize
-    output += '\n'+'Key: ' + key
-    output += '\n'+'Clear text: \n' + clearText
-
-    FileUtils.write(file, output)
+    while(true) {
+        key = readline.question(
+                          chalk.red.bold('\n---Decriptador de Cifras de Vegenère---') +
+                          '\n(para sair: ctrl + c)' +
+                          '\nChave mais provável                     : '+ chalk.green.bold(possibleKeys.bestKeyCandidate) + 
+                          '\nCaracteres substitutos para cada posição: ' + chalk.green.bold(possibleKeys.keyCharactersAlternatives)+
+                          '\nTentar chave:')
+        clearText = getClearText(cipherText, key);
+        console.log('Texto claro: \n' + clearText)
+        console.log('Tamanho da chave: ' + key.length)
+        console.log('Chave: ' + key)
+        output = ''
+        output += 'Tamanho da chave: ' + key.length
+        output += '\nChave: ' + key
+        output += '\nTexto claro: \n' + clearText
+        FileUtils.write(file, output)
+        console.log('\nTexto gravado em '+'./clear-text/'+file+'.clear')
+    }
 }
 
 
@@ -73,7 +96,7 @@ const calculatekeySize = (cipherText) => {
 
     for(var [i, v] of table.entries()) {
         for(var index in v ) {
-            indexDistance = Math.abs(englishCoincidenceIndex - v[index].index)
+            indexDistance = Math.abs(languageCoincidenceIndex - v[index].index)
             indexDistance < closest ? (
                 keySize = i, closest = indexDistance
             ) : null
@@ -84,8 +107,32 @@ const calculatekeySize = (cipherText) => {
 }
 
 
-const discoverKey = (cipherText, keySize) => {
-    let column = '', swapCiphers = [], key = '', mostFrequentLetter
+const getClearText = (cipherText, key) => {
+    let clearText = '', j = 0, cipherLetterIndex = 0, keyLetterIndex = 0, clearTextLetterIndex = 0
+    for(let i=0; i< cipherText.length; i++) {
+        cipherLetterIndex = alphabet.indexOf(cipherText.charAt(i))
+        keyLetterIndex = alphabet.indexOf(key.charAt(j))
+        clearTextLetterIndex = keyLetterIndex > cipherLetterIndex ? Math.abs(alphabet.length - keyLetterIndex + cipherLetterIndex) : Math.abs(keyLetterIndex - cipherLetterIndex)
+        
+        clearText += alphabet.charAt(clearTextLetterIndex)
+        
+        j++
+        if(j == key.length) {
+            j = 0    
+        }
+    }
+    return clearText
+}
+
+
+
+const discoverPossibleKeys = (cipherText, keySize) => {
+    let column = '',
+        swapCiphers = [],
+        keyAlphaet = [],
+        bestKeyCandidate = '',
+        keyCharactersAlternatives = ''
+        
 
     for(var i=0; i<keySize; i++) {
         for(var j=i; j<=cipherText.length; j+=keySize) {
@@ -97,30 +144,27 @@ const discoverKey = (cipherText, keySize) => {
 
     for(let cipher in swapCiphers) {
         lettersOccurrences = CoincidenceIndexService.calculateFrequencyOccurrences(swapCiphers[cipher])
-        mostFrequentLetter = Object.keys(lettersOccurrences).reduce(function(a, b){ return lettersOccurrences[a] > lettersOccurrences[b] ? a : b });
-        key += alphabet.charAt( Math.abs(alphabet.indexOf(mostFrequentLetter) - alphabet.indexOf(englishMostFrequentLetter)) )
+        orderedLettersOccurrences = getOrderedLettersOccurrences(lettersOccurrences)
+
+        bestKeyCandidate += alphabet.charAt( Math.abs(alphabet.indexOf(orderedLettersOccurrences[0].letter) - alphabet.indexOf(languageMostFrequentLetter)) )
+        keyCharactersAlternatives += alphabet.charAt( Math.abs(alphabet.indexOf(orderedLettersOccurrences[1].letter) - alphabet.indexOf(languageMostFrequentLetter)) )
+            
     }
 
-    return key
+    return {bestKeyCandidate: bestKeyCandidate, keyCharactersAlternatives: keyCharactersAlternatives}
 }
 
-
-const getClearText = (cipherText, key) => {
-    let clearText = '', j = 0, cipherLetterIndex = 0, keyLetterIndex = 0, clearTextLetterIndex = 0
-    for(let i=0; i< cipherText.length; i++) {
-        cipherLetterIndex = alphabet.indexOf(cipherText.charAt(i))
-        keyLetterIndex = alphabet.indexOf(key.charAt(j))
-        clearTextLetterIndex = Math.abs( cipherLetterIndex - keyLetterIndex )
-        clearTextLetterIndex = keyLetterIndex > cipherLetterIndex ? Math.abs(alphabet.length - keyLetterIndex + cipherLetterIndex) : Math.abs(keyLetterIndex - cipherLetterIndex)
-        
-        clearText += alphabet.charAt(clearTextLetterIndex)
-        
-        j++
-        if(j == key.length) {
-            j = 0    
+const getOrderedLettersOccurrences = (lettersOccurrences) => {
+    orderedLettersOccurrences = []
+    Object.keys(lettersOccurrences).forEach(
+        function(a){ orderedLettersOccurrences.push({letter: a, frequency: lettersOccurrences[a]}) }
+    )
+    orderedLettersOccurrences = orderedLettersOccurrences.sort(
+        function(a, b){
+            return a.frequency > b.frequency ? -1 : 1
         }
-    }
-    return clearText
+    )
+    return orderedLettersOccurrences
 }
 
 
